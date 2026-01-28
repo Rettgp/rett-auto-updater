4444663e06954db5b1bc07c83b64583a57984018
Garrett Phelps <garrphelps@gmail.com>
2026-01-28 17:01:13 -0600

    Rename

--- rett-auto-updater.lua
+++ rett-auto-updater.lua
@@ -1,11 +1,11 @@
--- bb-auto-updater.lua
+-- rett-auto-updater.lua
 -- Windower addon: Automatically updates specified addons based on a remote manifest
 -- Author: Rettgp
 
-_addon.name = 'bb-auto-updater'
+_addon.name = 'rett-auto-updater'
 _addon.version = '1.0.0'
 _addon.author = 'Rettgp'
-_addon.commands = {'bbautoupdater', 'bbau'}
+_addon.commands = {'rettautoupdater', 'rettau'}
 
 local json = require('json')
 local http = require('socket.http')
@@ -40,11 +40,11 @@ end
 
 local function update_addon(addon_name, download_url)
     -- TODO: Download and extract the addon, replace local folder
-   notice(207, ('[bb-auto-updater] Would update %s from %s'):format(addon_name, download_url))
+   notice(207, ('[rett-auto-updater] Would update %s from %s'):format(addon_name, download_url))
 end
 
 windower.register_event('load', function()
-    windower.add_to_chat(207, '[bb-auto-updater] Checking for addon updates...')
+    windower.add_to_chat(207, '[rett-auto-updater] Checking for addon updates...')
     local manifest_data = {}
     local response = {}
     local _, code = http.request{
@@ -52,28 +52,28 @@ windower.register_event('load', function()
         sink = ltn12.sink.table(response)
     }
     if code ~= 200 then
-        error(123, '[bb-auto-updater] Failed to fetch manifest: HTTP ' .. tostring(code))
+        error(123, '[rett-auto-updater] Failed to fetch manifest: HTTP ' .. tostring(code))
         return
     end
     local manifest_json = table.concat(response)
     local success, manifest = pcall(json.decode, manifest_json)
     if not success or not manifest then
-        error(123, '[bb-auto-updater] Failed to parse manifest.json')
+        error(123, '[rett-auto-updater] Failed to parse manifest.json')
         return
     end
     for addon_name, remote_info in pairs(manifest.addons or {}) do
         local local_version = get_local_version(addon_name)
         if local_version and local_version ~= remote_info.version then
-            windower.add_to_chat(207, ('[bb-auto-updater] %s is outdated (local: %s, remote: %s)'):format(addon_name, local_version, remote_info.version))
+            windower.add_to_chat(207, ('[rett-auto-updater] %s is outdated (local: %s, remote: %s)'):format(addon_name, local_version, remote_info.version))
             update_addon(addon_name, remote_info.download_url or ADDONS_BASE_URL)
         end
     end
-    notice(207, '[bb-auto-updater] Update check complete.')
+    notice(207, '[rett-auto-updater] Update check complete.')
 end)
 
 -- Command: Manual update check
 windower.register_event('addon command', function(cmd)
     if cmd == 'check' then
-        windower.send_command('lua reload bb-auto-updater')
+        windower.send_command('lua reload rett-auto-updater')
     end
 end)
